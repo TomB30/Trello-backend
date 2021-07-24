4f1470b21607b7172917f86b7af098d093dfbb24
Tom Bechar <tomchick30@gmail.com>
2021-07-24 20:00:42 +0300

    half socket

--- services/socket.service.js
+++ services/socket.service.js
@@ -33,11 +33,14 @@ function connectSockets(http, session) {
         })
 
         socket.on('user-watch', userId => {
-            socket.join('watching:' + userId)
-        })
-        socket.on('set-user-socket', userId => {
-            logger.debug(`Setting socket.userId = ${userId}`)
+            if(socket.userId) socket.leave(socket.userId)
             socket.userId = userId
+            socket.join(userId)
+            console.log('watching ', userId );
+        })
+        socket.on('send user', user =>{
+            console.log('updating user',user);
+            gIo.to(user._id).emit('user updated', user)
         })
         socket.on('unset-user-socket', () => {
             delete socket.userId
